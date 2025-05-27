3498b0b71d16d8e3715d395391b8e10f2e515af4
VaibhavPrabhakarPatil <vaibhavpatil25112@gmail.com>
2025-05-27 16:24:03 +0530

    Updated selenium script with iframe handling and Job Support click and Pytest

--- 1st_seleninum.py
+++ 1st_seleninum.py
@@ -125,22 +125,142 @@ import time
 
 #Pop Alert Example
 
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# import time
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# drive.find_element(By.XPATH,"//input[@id='name']").send_keys("vaibhav patil")
+# time.sleep(4)
+# drive.find_element(By.XPATH,"//input[@id='alertbtn']").click()
+# time.sleep(4)
+# popup = drive.switch_to.alert
+# print("Alert says:", popup.text)
+# popup.accept()
+# time.sleep(4)
+
+#file upload testing & implicit-wait example
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.implicitly_wait(10)
+# drive.get("https://the-internet.herokuapp.com/upload")
+# drive.find_element(By.XPATH,("//input[@id='file-upload']")).send_keys("C:\\Users\\vaibh\\OneDrive\\Pictures\\Screenshots\\Screenshot 2024-11-05 174344.png")
+# time.sleep(4)
+# drive.find_element(By.XPATH,"//input[@class='button']").click()
+
+#mouse Action Testing & ActionChains Example
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver import ActionChains
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# action = ActionChains(drive)
+# mouse_hover_button = drive.find_element(By.XPATH,"//button[@id='mousehover']")
+# action.move_to_element(mouse_hover_button).perform()
+# top_link = drive.find_element(By.LINK_TEXT,"Top")
+# action.move_to_element(top_link).click().perform()
+
+# mouse hover and Double Click and Right Click Example
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver import ActionChains
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.get("https://the-internet.herokuapp.com/context_menu")
+# action = ActionChains(drive)
+# Double_Click = drive.find_element(By.XPATH,"//div[@id='hot-spot']")
+# action.context_click(Double_Click).perform() # context_Click means right click
+# action.double_click(Double_Click).perform()
+
+# Multiple Windows Handling Example
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# import time
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# drive.find_element(By.XPATH,"//button[@id='openwindow']").click()
+# print(drive.window_handles)
+# time.sleep(2)
+# drive.switch_to.window(drive.window_handles[1])
+# drive.find_element(By.XPATH,"//li[@class='nav-item']//a[@href='about.html'][normalize-space()='About us']").click()
+# drive.switch_to.window(drive.window_handles[0])
+
+# Multiple Tab handling Example
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.implicitly_wait(4)
+# drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# drive.find_element(By.XPATH,"//a[@id='opentab']").click()
+# Tab = drive.window_handles[1]
+# drive.find_element(By.XPATH,"//button[@id='openwindow']").click()
+# window2 = drive.window_handles[2]
+# drive.switch_to.window(Tab)
+# drive.find_element(By.XPATH,"//li[@class='nav-item']//a[@href='about.html'][normalize-space()='About us']").click()
+# drive.switch_to.window(window2)
+# drive.find_element(By.XPATH,"//a[@class='active']").click()
+
+# Frames
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# import time
+#
+# service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
+# drive = webdriver.Chrome(service=service)
+# drive.implicitly_wait(20)
+# drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+# drive.switch_to.frame(drive.find_element(By.XPATH,"//iframe[@id='courses-iframe']"))
+# drive.find_element(By.XPATH,"//div[@class='nav-outer clearfix']//nav[@class='main-menu']//div[@class='navbar-collapse collapse clearfix']//ul[@class='navigation clearfix']//li//a[@href='consulting'][normalize-space()='Job Support']").click()
+# time.sleep(10)
+
+# get Attribute Example means ex of placeholder name copy
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
 
 service = Service("C:\\Users\\vaibh\\Downloads\\chromedriver-win64\\chromedriver.exe")
 drive = webdriver.Chrome(service=service)
 drive.get("https://www.rahulshettyacademy.com/AutomationPractice/")
-drive.find_element(By.XPATH,"//input[@id='name']").send_keys("vaibhav patil")
-time.sleep(4)
-drive.find_element(By.XPATH,"//input[@id='alertbtn']").click()
-time.sleep(4)
-popup = drive.switch_to.alert
-print("Alert says:", popup.text)
-popup.accept()
-time.sleep(4)
+placeholdernamecopy = drive.find_element(By.XPATH,"//input[@id='name']").get_attribute("placeholder")
+print(placeholdernamecopy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ test_1stcode.py
@@ -0,0 +1,5 @@
+def test_1stcode():
+    print('this is my 1st test')
+
+def test_2stcode():
+    print('this is my 2st test')
